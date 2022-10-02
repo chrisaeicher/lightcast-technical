4182162b53b871fe1c562f18b1147b0b1a5e15ad
Chris E <chrisaeicher@gmail.com>
2022-10-02 00:07:47 -0600

    Added an option to load local sample data if API fetch fails.

--- src/components/PageContent/PageContent.jsx
+++ src/components/PageContent/PageContent.jsx
@@ -7,6 +7,11 @@ export default function PageContent({pageHeading="Occupation Overview" }) {
     const [occupationData, setOccupationData] = useState(null);
     const [failedToLoad, setFailedToLoad] = useState(false);
 
+    const loadSampleData = () => {
+        setOccupationData(backupData);
+        setFailedToLoad(false);
+    }
+
     const fetchOccupationData = async () => {
         try {
             const res = await fetch('https://run.mocky.io/v3/a2cc3707-8691-4188-8413-6183a7bb3d32');
@@ -25,7 +30,7 @@ export default function PageContent({pageHeading="Occupation Overview" }) {
 
     return (
         <main>
-            {occupationData && 
+            {!failedToLoad && occupationData && 
             <>
             <h1 className="text-3xl font-medium">{pageHeading}</h1>
             <h2 className="mb-5">{occupationData.occupation.title} in {occupationData.region.title}</h2>
@@ -33,7 +38,15 @@ export default function PageContent({pageHeading="Occupation Overview" }) {
             <TrendChart />
             <IndustryBreakdown />
             </>
-        }
+        } 
+        {failedToLoad &&
+        <div className="flex flex-col items-center">
+            <h1 className="text-3xl mx-autotext-center-font-semibold">I'm sorry, we weren't able to retrieve the requested information.</h1>
+            <div className="flex gap-4">
+            <button className="mt-5 px-4 py-2 bg-gray-700 hover:bg-green-600 transition text-white rounded" onClick={loadSampleData}>Load sample data</button>
+            <button className="mt-5 px-4 py-2 transition text-gray-700 rounded hover:bg-green-100" onClick={() => window.location = "/"}>Refresh the page</button>
+            </div>
+        </div>}
         </main>
     )
 }
--- src/components/PageContent/components/OccupationSummary/components/PercentChangeCard.jsx
+++ src/components/PageContent/components/OccupationSummary/components/PercentChangeCard.jsx
@@ -3,9 +3,9 @@ export default function PercentChangeCard({startYear, endYear, regional, nationa
     const isPositiveNationalGrowth = nationalGrowth > 0;
     return (
         <div className="flex flex-col align-center text-center py-5">
-        <h4 className={isPositiveRegionalGrowth ? "text-3xl mb-1 text-green-600" : "text-3xl mb-1 text-red-600"}>{isPositiveRegionalGrowth ? "+" : "-"}{regional}%</h4>
+        <h4 className={isPositiveRegionalGrowth ? "text-3xl mb-1 text-green-600" : "text-3xl mb-1 text-red-600"}>{isPositiveRegionalGrowth ? "+" : ""}{regional}%</h4>
         <h5 className="text-sm font-medium">% Change ({startYear}-{endYear})</h5>
-        <p className="text-sm">Nation: <span className={isPositiveNationalGrowth ? "text-green-600" : "text-red-600"}>{isPositiveNationalGrowth ? "+" : "-"}{nationalGrowth}%</span></p>
+        <p className="text-sm">Nation: <span className={isPositiveNationalGrowth ? "text-green-600" : "text-red-600"}>{isPositiveNationalGrowth ? "+" : ""}{nationalGrowth}%</span></p>
     </div>
     )
 }
